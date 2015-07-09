21379e4f1e7ff08c1ec6670d3aa1826aa8973081
naveenwashere <naveenwashere@gmail.com>
2015-07-09 22:06:36 +0530

    Assign feature.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,29 +9,36 @@ import models.*;
 public class Application extends Controller {
   static Form<Task> taskForm = Form.form(Task.class); 
   
-  public static Result index() {
+  public static Result index() 
+  {
     return redirect(routes.Application.tasks());
   }
   
-  public static Result tasks() {
+  public static Result tasks() 
+  {
     return ok(
       views.html.index.render(Task.all(), taskForm)
     );
   }
   
-  public static Result newTask() {
+  public static Result newTask() 
+  {
     Form<Task> filledForm = taskForm.bindFromRequest();
-      if(filledForm.hasErrors()) {
+      if(filledForm.hasErrors()) 
+      {
         return badRequest(
           views.html.index.render(Task.all(), filledForm)
         );
-      } else {
+      } 
+      else 
+      {
         Task.create(filledForm.get());
         return redirect(routes.Application.tasks());  
       }
   }
   
-  public static Result deleteTask(String id) {
+  public static Result deleteTask(String id) 
+  {
     Task.delete(id);
     return redirect(routes.Application.tasks());
   }
--- app/models/Task.java
+++ app/models/Task.java
@@ -7,7 +7,8 @@ import net.vz.mongodb.jackson.JacksonDBCollection;
 import net.vz.mongodb.jackson.ObjectId;
 import play.modules.mongodb.jackson.MongoDB;
 
-public class Task{
+public class Task
+{
     
   @Id
   @ObjectId
@@ -20,39 +21,48 @@ public class Task{
   public String createdBy;
 
   public String status;
-
+  
   private static JacksonDBCollection<Task, String> coll = MongoDB.getCollection("tasks", Task.class, String.class);
-
-  public Task(){
+  
+  public Task()
+  {
 
   }
 
-  public Task(String customerInfo, String comments, String createdBy, String status) {
+  public Task(String customerInfo, String comments, String createdBy, String status) 
+  {
     this.customerInfo = customerInfo;
     this.comments = comments;
     this.createdBy = createdBy;
     this.status = status;
   }
 
-  public static List<Task> all() {
+  public static List<Task> all() 
+  {
     return Task.coll.find().toArray();
   }
 
-  public static void create(Task task) {
+  public static void create(Task task) 
+  {
     Task.coll.save(task);
   }
 
-  public static void create(String customerInfo, String comments, String createdBy, String status){
+  public static void create(String customerInfo, String comments, String createdBy, String status)
+  {
       create(new Task(customerInfo, comments, createdBy, status));
   }
 
-  public static void delete(String id) {
+  public static void delete(String id) 
+  {
     Task task = Task.coll.findOneById(id);
     if (task != null)
+    {
         Task.coll.remove(task);
+    }
   }
 
-  public static void removeAll(){
+  public static void removeAll()
+  {
     Task.coll.drop();
   }
 
