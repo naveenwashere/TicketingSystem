8b0727ba755c822d80f268418711281ed468331e
naveenwashere <naveenwashere@gmail.com>
2015-07-09 21:08:17 +0530

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+What's new:
+
+- Updated scala version to 2.11.1
+
+First steps with Playframework 2.3.1 (Java) and MongoDb (Jackson)
+--------------------------------------------------
+
+* Don't forget to start mongodb daemon! It should use the default host (localhost) and the default port.
+
+* Run the application:
+```
+sbt run
+```
+
+Please report if there is any issue with running this tutorial, thanks!
---
+++ app/models/Task.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.List;
+
+import net.vz.mongodb.jackson.Id;
+import net.vz.mongodb.jackson.JacksonDBCollection;
+import net.vz.mongodb.jackson.ObjectId;
+import play.modules.mongodb.jackson.MongoDB;
+
+public class Task{
+    
+  @Id
+  @ObjectId
+  public String id;
+  
+  public String customerInfo;
+
+  public String comments;
+  
+  public String createdBy;
+
+  public String status;
+
+  private static JacksonDBCollection<Task, String> coll = MongoDB.getCollection("tasks", Task.class, String.class);
+
+  public Task(){
+
+  }
+
+  public Task(String customerInfo, String comments, String createdBy, String status) {
+    this.customerInfo = customerInfo;
+    this.comments = comments;
+    this.createdBy = createdBy;
+    this.status = status;
+  }
+
+  public static List<Task> all() {
+    return Task.coll.find().toArray();
+  }
+
+  public static void create(Task task) {
+    Task.coll.save(task);
+  }
+
+  public static void create(String customerInfo, String comments, String createdBy, String status){
+      create(new Task(customerInfo, comments, createdBy, status));
+  }
+
+  public static void delete(String id) {
+    Task task = Task.coll.findOneById(id);
+    if (task != null)
+        Task.coll.remove(task);
+  }
+
+  public static void removeAll(){
+    Task.coll.drop();
+  }
+
+}
