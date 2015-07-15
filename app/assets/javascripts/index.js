c9123f1ad74fdb25b39ce9de35e5ac8c884cd312
naveenwashere <naveenwashere@gmail.com>
2015-07-15 18:22:22 +0530

    Change CSS layouts based on feedback.

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,90 @@
+var app = angular.module('myApp', []);
+
+app.controller('appCtrl', function($scope, $http) {
+  $http.get('/users').success(function(response) {
+    $scope.users = response;
+  });
+  
+  $scope.toggleAdd = function() {
+    $scope.showView = false;
+    $scope.assignView = false;
+    $scope.assignFields = false;
+    $scope.updateView = false;
+    $scope.status = 'New';
+    $scope.addView = !$scope.addView;
+  };
+  
+  $scope.toggleShow = function() {
+    $scope.addView = false;
+    $scope.assignView = false;
+    $scope.assignFields = false;
+    $scope.updateView = false;
+    $scope.showView = !$scope.showView;
+  };
+  
+  $scope.toggleAssign = function() {
+    $scope.addView = false;
+    $scope.showView = false;
+    $scope.assignFields = false;
+    $scope.updateView = false;
+    $scope.assignView = !$scope.assignView;
+  };
+  
+  $scope.toggleAssignTicket = function(id, custInfo, comment, originator) {
+    $scope.assignId = id;
+    $scope.assignCustomerInfo = custInfo;
+    $scope.assignComments = comment;
+    $scope.assignCreatedBy = originator;
+    $scope.assignStatus = 'Open';
+    $scope.addView = false;
+    $scope.showView = false;
+    $scope.assignView = false;
+    $scope.updateView = false;
+    $scope.assignFields = !$scope.assignFields;
+  };
+  
+  $scope.toggleUpdateTicket = function(id, custInfo, comment, originator, status, assignTo) {
+    $scope.updateId = id;
+    $scope.updateCustomerInfo = custInfo;
+    $scope.updateComments = comment;
+    $scope.updateCreatedBy = originator;
+    $scope.updateStatus = status;
+    $scope.updateAssignTo = assignTo;
+    $scope.editable = false;
+    if ($scope.updateStatus === 'Close') {
+      $scope.editable = true;
+    }
+    $scope.addView = false;
+    $scope.showView = false;
+    $scope.assignView = false;
+    $scope.updateView = !$scope.updateView;
+  };
+  
+  $scope.closeAllViews = function() {
+    $scope.addView = false;
+    $scope.showView = false;
+    $scope.assignView = false;
+    $scope.updateView = false;
+    $scope.assignFields = false;
+  };
+  
+  $scope.toggleSearch = function(id) {
+    if (typeof id === 'undefined') {
+      alert('Please enter a valid ticket ID.');
+    }
+    $scope.closeAllViews();
+    $http.get('/tickets/' + id + '/searchById').success(function(response) {
+      var obj;
+      $scope.ticket = response;
+      obj = JSON.parse(JSON.stringify($scope.ticket));
+      $scope.toggleUpdateTicket(obj.id, obj.customerInfo, obj.comments, obj.createdBy, obj.status, obj.assignTo);
+    });
+  };
+  
+  $scope.deleteTicket = function(id) {
+    $http.post('/tickets/' + id + '/delete').success(function(response) {
+      $scope.ticket = response;
+      location.reload(true);
+    });
+  };
+});
